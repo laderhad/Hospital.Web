df846e81661762e90b438f11992107b13fddf7b7
Utku Kerem <68512069+laderhad@users.noreply.github.com>
2025-01-13 16:54:31 +0300

    generics and migration has been created

---
+++ Hospital.Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+        void Save();
+
+
+    }
+}
--- Hospital.Web/Program.cs
+++ Hospital.Web/Program.cs
@@ -1,6 +1,10 @@
 using Hospital.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Hospital.Utilities;
+using Hospital.Repositories.Interfaces;
+using Hospital.Repositories.Implemantation;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();   
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -23,14 +31,23 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+DataSeeding();
 app.UseRouting();
 app.UseAuthentication();;
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{Area=Patient}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void DataSeeding()
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
