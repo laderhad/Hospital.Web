4968d5301167dbaff92b67c13e99a930812eff77
Utku Kerem <68512069+laderhad@users.noreply.github.com>
2025-01-13 15:25:27 +0300

    arch has been created

---
+++ Hospital.Utilities/WebSiteRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Utilities
+{
+    public static class WebSiteRoles
+    {
+        public const string WebSite_Admin = "Admin";
+        public const string WebSite_Patient = "Patient";
+        public const string WebSite_Doctor = "Doctor";
+    }
+}
