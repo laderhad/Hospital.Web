1a39eeb7ddb9e9129a43a413a0750295ee4daa78
laderhad <laderhad@gmail.com>
2025-01-14 22:10:44 +0300

    Service has been created

---
+++ Hospital.Web/Areas/Admin/Controllers/HospitalCOntroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Web.Areas.Admin.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
