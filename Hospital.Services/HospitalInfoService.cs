122b635d46a4fbbaa4091e6dd1328a345c024a98
laderhad <laderhad@gmail.com>
2025-01-14 21:36:55 +0300

    Hospital service has been created

---
+++ Hospital.Services/HospitalInfoService.cs
@@ -0,0 +1,81 @@
+﻿using Hospital.Models;
+using Hospital.Repositories.Interfaces;
+using Hospital.Utilities;
+using Hospital.ViewModels;
+
+namespace Hospital.Services
+{
+    public class HospitalInfoService : IHospitalInfo
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public HospitalInfoService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void DeleteHospitalInfo(int HospitalId)
+        {
+            var model = _unitOfWork.Repository<HospitalInfo>().GetByID(HospitalId);
+
+        }
+
+        public PagedResult<HospitalInfoViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            var vm = new HospitalInfoViewModel();
+            int totalCount;
+            List<HospitalInfoViewModel> vmList = new List<HospitalInfoViewModel>();
+            try
+            {
+                int excludeRecords = (pageSize * pageNumber) - pageSize;
+                var modelList = _unitOfWork.Repository<HospitalInfo>().GetAll().Skip(excludeRecords).Take(pageSize).ToList();
+                totalCount = _unitOfWork.Repository<HospitalInfo>().GetAll().Count();
+                vmList = ConvertModelToViewModelList(modelList);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            var result = new PagedResult<HospitalInfoViewModel>
+            {
+                Data = vmList,
+                TotalItems = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+            return result;
+        }
+
+        public HospitalInfoViewModel GetHospitalById(int HospitalId)
+        {
+            var model = _unitOfWork.Repository<HospitalInfo>().GetByID(HospitalId);
+            var vm =new HospitalInfoViewModel(model);
+            return vm;
+        }
+
+        public void InsertHospitalInfo(HospitalInfoViewModel hospitalInfoViewModel)
+        {
+            var model = new HospitalInfoViewModel().ConvertViewModel(hospitalInfoViewModel);
+            _unitOfWork.Repository<HospitalInfo>().Add(model);
+            _unitOfWork.Save();
+        }
+
+        public void UpdateHospitalInfo(HospitalInfoViewModel hospitalInfo)
+        {
+            var model = new HospitalInfoViewModel().ConvertViewModel(hospitalInfo);
+            var ModelById=_unitOfWork.Repository<HospitalInfo>().GetByID(model.Id);
+            ModelById.Name = hospitalInfo.Name;
+            ModelById.Country = hospitalInfo.Country;
+            ModelById.City = hospitalInfo.City;
+            ModelById.PinCode = hospitalInfo.PinCode;
+            _unitOfWork.Repository<HospitalInfo>().Update(model);
+            _unitOfWork.Save();
+        }
+
+        private List<HospitalInfoViewModel> ConvertModelToViewModelList(List<HospitalInfo> modelList)
+        {
+            return modelList.Select(x => new HospitalInfoViewModel(x)).ToList();
+        }
+    }
+}
